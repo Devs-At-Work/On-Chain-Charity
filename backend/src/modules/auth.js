cd50effe322c82f8a3f143c7a0e0e0a38f9a33bb
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-10-28 13:26:34 +0530

    user creation; user siginin

--- backend/src/modules/auth.js
+++ backend/src/modules/auth.js
@@ -1,7 +1,53 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 export const comparePassword = async (password, hashedPassword) => {
     return bcrypt.compare(password, hashedPassword);
 }
 
+export const hashPassword = async (password) => {
+    return bcrypt.hash(password, 10);
+}
+
+export const createJWT = (user) => {
+    const token = jwt.sign({
+        id: user.id,
+        username: user.username,
+        email: user.email
+    },
+        process.env.JWT_SECRET
+    )
+    return token
+}
+
+export const protect = async (req, res, next) => {
+    const bearerToken = req.headers.authorization;
+
+    if (!bearerToken) {
+        res.status(401).json({
+            message: 'not authorized'
+        })
+        return
+    }
+
+    const [, token] = bearerToken.split(' ');
+
+    if (!token) {
+        res.status(401).json({
+            message: 'not a valid token'
+        })
+        return
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = user;
+        next();
+    } catch (err) {
+        console.error(err);
+        res.status(401).json({
+            message: 'not a valid token'
+        })
+        return
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { protect } from './modules/auth.js';
+import router from './router.js';
+import { createNewUser, sigin } from './handlers/user.js';
 
 const app = express();
 
@@ -16,5 +19,19 @@ app.get('/', (req, res) => {
         message: "Hello World"
     })
 });
+
+app.use('/api', protect, router);
+app.use('/user', createNewUser);
+app.use('/signin', sigin);
+
+app.use((err, req, res, next) => {
+    if (err.type === 'auth') {
+        res.status(401).json({ message: 'unauthorized' })
+    } else if (err.type === 'input') {
+        res.status(400).json({ message: 'bad request' })
+    } else {
+        res.status(500).json({ message: 'internal server error' })
+    }
+})
 
 export default app;
