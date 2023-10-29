c9289db06abe06a6c5af6df9aa27657c3057bd87
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-10-29 18:09:59 +0530

    adding projects handlers

---
+++ backend/src/handlers/projects.js
@@ -0,0 +1,82 @@
+import prisma from "../configs/db.js";
+
+// GET all projects
+export const getAllProjects = async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: req.user.id
+        },
+        include: {
+            projects: true
+        }
+    })
+
+    res.json({
+        data: user.projects
+    })
+}
+
+// GET project by id
+export const getProjectById = async (req, res) => {
+    const project = await prisma.project.findFirst({
+        where: {
+            id: req.params.id,
+            belongsToID: req.user.id
+        }
+    })
+
+    res.json({
+        data: project
+    })
+}
+
+// POST a new project
+export const createNewProject = async (req, res) => {
+    const project = await prisma.project.create({
+        data: {
+            name: req.body.name,
+            description: req.body.description,
+            belongsToID: req.user.id
+        }
+    })
+
+    res.json({
+        data: project
+    })
+}
+
+// PUT updated data in a existing project
+export const updateProject = async (req, res) => {
+    const updated = await prisma.project.update({
+        where: {
+            id_belongsToID: {
+                id: req.params.id,
+                belongsToID: req.user.id
+            }
+        },
+        data: {
+            name: req.body.name,
+            description: req.body.description
+        }
+    })
+
+    res.json({
+        data: updated
+    })
+}
+
+// DELETE an existing project
+export const deleteProject = async (req, res) => {
+    const deleted = await prisma.project.delete({
+        where: {
+            id_belongsToID: {
+                id: req.params.id,
+                belongsToID: req.user.id
+            }
+        }
+    })
+
+    res.json({
+        data: deleted
+    })
+}
