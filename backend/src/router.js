bd32b574ff2cf6c1d3651036698eb617f684adee
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-10-29 18:28:43 +0530

    updating routes for projects

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,21 +1,23 @@
 import { Router } from "express";
 import { body } from "express-validator";
+import { handleInputErrors } from "./modules/middleware.js";
+import { getProjects, getProject, createProject, updateProject, deleteProject } from "./modules/project.js";
 
 const router = Router();
 
 // Routes for projects
 
-router.get('/project', )
+router.get('/project', getProjects)
 
-router.get('/project/:id',)
+router.get('/project/:id', getProject)
 
-router.post('/project', )
+router.post('/project', body('name').isString(), handleInputErrors, createProject)
 
-router.put('/project/:id', )
+router.put('/project/:id', body('name').isString(), handleInputErrors, updateProject)
 
-router.delete('/project/:id', )
+router.delete('/project/:id', deleteProject)
 
-router.use((err,req,res,next)=>{
+router.use((err, req, res, next) => {
     console.error(err);
 })
 
