b87ff15b75aa2e854eae37bcd46b30eba21031ba
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-10-29 18:21:04 +0530

    unit test for user.js

---
+++ backend/src/handlers/__tests__/user.test.js
@@ -0,0 +1,21 @@
+import * as user from '../user.js';
+
+describe('user hanlder', () => {
+    it('should create a new user', async () => {
+        const req = {
+            body: {
+                username: 'test',
+                password: 'test1234',
+                email: 'test@abc.com',
+            }
+        }
+        const res = {
+            json({ token }) {
+                console.log(token)
+                expect(token).toBeTruthy()
+            }
+        }
+
+        await user.createNewUser(req, res, () => { });
+    })
+})
