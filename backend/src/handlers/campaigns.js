9d43e0afb670a28183cc67754bb484cff30cc722
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-12-05 21:18:08 +0530

    adding modifications to campaign model and handlers

--- backend/src/handlers/campaigns.js
+++ backend/src/handlers/campaigns.js
@@ -2,81 +2,140 @@ import prisma from "../configs/db.js";
 
 // GET all campaigns
 export const getAllUserCampaigns = async (req, res) => {
-    const user = await prisma.user.findUnique({
-        where: {
-            id: req.user.id
-        },
-        include: {
-            campaigns: true
-        }
-    })
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.user.id
+            },
+            include: {
+                campaigns: true
+            }
+        })
 
-    res.json({
-        data: user.campaigns
-    })
+        res.json({
+            data: user.campaigns
+        })
+    } catch (err) {
+        console.log('Error getting user campaigns: ', err);
+        return res.status(500).json({
+            error: 'Internal server error.'
+        });
+    }
 }
 
 export const getAllCampaigns = async (req, res) => {
-    const campaigns = await prisma.campaign.findMany({
-        include: {
-            belongsTo: true
-        }
-    })
+    try {
+        const campaigns = await prisma.campaign.findMany({
+            include: {
+                belongsTo: true
+            }
+        })
 
-    res.json({
-        data: campaigns
-    })
+        res.json({
+            data: campaigns
+        })
+    } catch (err) {
+        console.log('Error getting campaigns: ', err);
+        return res.status(500).json({
+            error: 'Internal server error.'
+        });
+    }
 }
 
 // GET campaign by id
 export const getCampaign = async (req, res) => {
-    const campaign = await prisma.campaign.findFirst({
-        where: {
-            id: req.params.id,
-            belongsToID: req.user.id
-        }
-    })
+    try {
+        const campaign = await prisma.campaign.findFirst({
+            where: {
+                id: req.params.id,
+                belongsToID: req.user.id
+            }
+        })
 
-    res.json({
-        data: campaign
-    })
+        res.json({
+            data: campaign
+        })
+    } catch (err) {
+        console.log('Error getting campaign: ', err);
+        return res.status(500).json({
+            error: 'Internal server error.'
+        });
+    }
 }
 
 // POST a new campaign
 export const createCampaign = async (req, res) => {
-    const campaign = await prisma.campaign.create({
-        data: {
-            name: req.body.name,
-            description: req.body.description,
-            belongsToID: req.user.id,
-            deadline: req.body.deadline
-        }
-    })
+    try {
+        const campaign = await prisma.campaign.create({
+            data: {
+                name: req.body.name,
+                description: req.body.description,
+                belongsToID: req.user.id,
+                deadline: req.body.deadline,
+                domain: req.body.domain,
+                location: req.body.location,
+                donationRequired: req.body.donationRequired,
+                peopleInvolved: req.body.peopleInvolved,
+            }
+        })
 
-    res.json({
-        data: campaign
-    })
+        res.json({
+            data: campaign
+        })
+    } catch (err) {
+        console.log('Error creating campaign: ', err);
+        return res.status(500).json({
+            error: 'Internal server error.'
+        });
+    }
 }
 
 // PUT updated data in a existing campaign
 export const updateCampaign = async (req, res) => {
-    const updated = await prisma.campaign.update({
-        where: {
-            id_belongsToID: {
+
+    try {
+        const isCreator = await prisma.campaign.count({
+            where: {
                 id: req.params.id,
                 belongsToID: req.user.id
-            }
-        },
-        data: {
-            name: req.body.name,
-            description: req.body.description,
-            deadline: req.body.deadline
-        }
-    })
+            },
+        });
 
-    res.json({
-        data: updated
-    })
+        if (isCreator == 0) {
+            return res.status(403).json({
+                error: 'Permission denied. User not the cretor of the campaign.'
+            });
+        }
+
+        const updated = await prisma.campaign.update({
+            where: {
+                id_belongsToID: {
+                    id: req.params.id,
+                    belongsToID: req.user.id
+                }
+            },
+            data: {
+                name: req.body.name,
+                description: req.body.description,
+                belongsToID: req.user.id,
+                deadline: req.body.deadline,
+                domain: req.body.domain,
+                location: req.body.location,
+                donationRequired: req.body.donationRequired,
+                peopleInvolved: req.body.peopleInvolved,
+            }
+        })
+
+        res.json({
+            data: updated
+        })
+    }
+    catch (err) {
+        console.log('Error updating campaign: ', err);
+        return res.status(500).json({
+            error: 'Internal server error.'
+        });
+    }
 }
 
 // DELETE an existing campaign
